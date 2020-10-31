f2b0c51801cc9d8a3ec08f88eef08bfd2d89d31a
Orion <ander2812@gmail.com>
2020-10-31 18:03:05 -0500

    'se crearon las clases y metodos'

---
+++ src/ui/Main.java
@@ -0,0 +1,15 @@
+package ui;
+
+import model.LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LinkedList lm = new LinkedList(3, 4);
+        System.out.println(lm);
+        char l = (int)'A';
+        int num = (l+1)-l;
+        System.out.println("el numero de A es: " + num);
+    }
+
+}
